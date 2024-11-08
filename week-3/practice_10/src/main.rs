ce4333706ba28a2a1366ce696922ab3a08c7b465
f.wuraolaCOS101 <fatimah.wuraola@pau.edu.ng>
2024-11-08 01:55:50 -0800

    Commit

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // addition
+    let sum = 5550 + 7310;
+    println!("The sum of 5550 and 7310 = {}",sum);
+
+    // subtraction
+    let difference:u32 = 955 - 43;
+    println!("The difference is 955 and 43 = {}",difference);
+
+    // multiplication
+    let product:f32 = 4.0 * 30.0;
+    println!("The product of 4 and 30 = {}",product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("The quotient of 56.7 and 32.2 = {}",quotient);
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("The remainder of 43 and 5 = {}",remainder);
+}
